f0f937b1d2a1715e0d8fd821d08183d8a078d437
root <root@ORION.lan>
2016-11-08 23:33:15 +0200

    Init

---
+++ HomeWork_Task_1.py
@@ -0,0 +1,5 @@
+lst = [1, [2, 3], 4, [[6, 7]]]
+lst_new = lst[0:1] + lst[1][0:1] + lst[1][1:2] + lst[2:3] + lst[3][0][0:1] + lst[3][0][:2]
+print (lst)
+print (lst_new)
+input()
---
+++ HomeWork_Task_2.py
@@ -0,0 +1,9 @@
+str = '((((((((((((((2, 3)))))))))))))'
+left_sk = str.count('(')
+right_sk = str.count(')')
+print ('Left bracket', left_sk)
+print ('Right bracket', right_sk, '\n')
+str_new = str + ')'
+print ('Old string', str)
+print ('New string', str_new)
+input()
