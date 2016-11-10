a471fd208371df381c77002c08ce2cb1d03df7c7
Sergiy <vir.host@gmail.com>
2016-11-10 17:17:57 +0200

    describe realization

--- HomeWork_Task_1.py
+++ HomeWork_Task_1.py
@@ -1,3 +1,10 @@
+"""It isn't valid realization. This must work authomaticaly.
+   When I send to to your program 'list' instance via argv
+   or via input/raw_input the output must be flaten list.
+   [1, 3, 5, [6, 10, 3, [2, 3], 3], 8]
+   ==>
+   [1, 3, 5, 6, 10, 3, 2, 3, 3, 8]
+"""
 lst = [1, [2, 3], 4, [[6, 7]]]
 lst_new = lst[0:1] + lst[1][0:1] + lst[1][1:2] + lst[2:3] + lst[3][0][0:1] + lst[3][0][:2]
 print (lst)
