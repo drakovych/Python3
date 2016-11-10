99fd1e903f631ee0b58ccb7f9b062acace241b2e
Sergiy <vir.host@gmail.com>
2016-11-10 17:23:39 +0200

    code style

--- HomeWork_Task_2.py
+++ HomeWork_Task_2.py
@@ -1,3 +1,7 @@
+"""Please don't user builtins for naming
+   'str', 'list', 'dict', 'set' etc. deprecated.
+   Always and your code files with new line.
+"""
 str = '((((((((((((((2, 3)))))))))))))'
 left_sk = str.count('(')
 right_sk = str.count(')')
