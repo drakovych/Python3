71962fd25f62d40c9867ebda142a46186b3a5696
drakovych <dima.rakovych@gmail.com>
2016-11-18 11:57:21 +0200

    Delete HomeWork_Task_1_1.py

--- HomeWork_Task_1_1.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-ListInList = [1, [2, 3], 4, [[6, 7]]]
-# ListInList = [[2, 3, 4], [6, 7]]
-# ListInList_new = list(itertools.chain.from_iterable(ListInList))
-
-my_set = set([1, 2, 3])
-print(my_set)
