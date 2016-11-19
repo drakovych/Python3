74b916ef11b91fb87ae0d81bcda1f2714408f695
drakovych <dima.rakovych@gmail.com>
2016-11-19 12:31:37 +0200

    Commit.7

---
+++ HomeWork_Task_2.2.py
@@ -0,0 +1,22 @@
+str = '((((((((((((((2, 3)))))))))))))'
+
+
+def brackets(old_string):
+    y = -1
+    for item in old_string:
+        if item == '(':
+            #            x += 1
+            while old_string[y] != ')':
+                print(old_string[y])
+                y -= 1
+                print(y)
+                if old_string[y] == '(':
+                    print('Close brackets \')\' !')
+                    break
+            else:
+                y -= 1
+                print('All ok!')
+
+
+brackets(str)
+input()
